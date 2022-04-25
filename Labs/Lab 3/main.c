20bba9f30419f591339209dbf69c05dc59d6060c
Eric Walsh <eric.walsh@wallawalla.edu>
2022-04-24 20:54:10 -0700

    Update main.c

--- Labs/Lab 3/main.c
+++ Labs/Lab 3/main.c
@@ -6,9 +6,10 @@
 // Compiler: Keil uVision5 																						   //
 // Library: CMSIS core and device startup 																		   //
 // also needs UART_Functions.h																					   //
-// Hardware: MKL25Z128VLH4 Microcontroller connected to 4 switches,a DAC, ADC, 1 RGB LEDS, and a 2x8 LCD  			   //
-// Expected Outcome: The UART2 channel would output the a char array data output into the UART2_Transmit function.				   //																									   //
-// Actual outcome: The UART2 channel succesfully sends data.													   //
+// Hardware: MKL25Z128VLH4 Microcontroller connected to 4 switches,a DAC, ADC, 1 RGB LEDS, and a 2x8 LCD  		   //
+// Expected Outcome: The scan_ADC funtion would scan 9 ADC channels and put the values into an array. The values in//
+// the array would then be output to the UART2 transmitter for debugging.	   									   //
+// Actual outcome: The scan function successfully scans the channels, however the UART2 is transmitting errors.    //
 // ****************************************************************************************************************/
 #include "MKL25z4.h"
 #include "UART_Functions.h"
